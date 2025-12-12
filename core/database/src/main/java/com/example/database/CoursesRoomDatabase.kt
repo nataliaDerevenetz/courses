979c6c51e61f8448e1e7a6b2c7f34ba0dceca711
Natalia Derevenetz <nna911@inbox.ru>
2025-12-12 15:55:21 +0300

    commit

--- core/database/src/main/java/com/example/database/CoursesRoomDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.database
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [CourseEntity::class], version = 1,exportSchema = false)
-abstract class CoursesRoomDatabase: RoomDatabase() {
-    abstract fun courseDao() : CourseDao
-}
